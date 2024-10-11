48514fb63deb1b3bebbae3073b69843aae999fd4
Vinod Vishwakarma <vkv.vinod@gmail.com>
2024-10-11 10:30:32 +0530

    chore: cypress update

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
+const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
+
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+
+    baseUrl: "https://www.saucedemo.com", //https://example.cypress.io
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+    configFile: 'reporter-config.json',
+    chromeWebSecurity: false,
     },
   },
+    setupNodeEvents(on, config) {
+      addMatchImageSnapshotPlugin(on, config);
+
+      // implement node event listeners here
+    },
 });
--- cypress/e2e/brokenLink.cy.js
+++ cypress/e2e/brokenLink.cy.js
@@ -1,5 +1,42 @@
-describe('Verify all broken link exit on page', () => {
-  it('Verify broken link', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Verify all broken link exits on the pages', () => {
+
+  // store all the pages in an array to check the broken link all togather
+  const pageToTestBrokenLink = ['/page1','/page2','/page3','/page4', '/page5'];
+  var brokenLinks =[];
+  pageToTestBrokenLink.forEach(page => {
+
+    it('Verify broken link', () => {
+      cy.visit('/')
+      //findout all links on everypage
+
+      cy.get('a[href]').each(($link)=>{
+        const href = $link.prop('href');
+        //verify if the link is valid
+        if(href && href.startWith('http')){
+          cy.request({
+            url: href,
+            failOnStatusCode:false,
+          }).then((response)=>{
+            if(response.status >= 400){
+              brokenLinks.push({ link: href, status: response.status });
+              cy.log(`Broken Links found: ${href} with status : ${response.status}`);
+            }else{
+              cy.log(`Valid Link with status: ${response.status} for - ${href}`);
+            }
+          })
+        }
+      })
+    });
+   })
+
+
+
+  after(() => {
+    if (brokenLinks.length) {
+      cy.writeFile('cypress/results/broken-links.json', brokenLinks);
+      cy.log('Broken links report generated:', brokenLinks);
+    } else {
+      cy.log('No broken links found.');
+    }
+  });
 })
